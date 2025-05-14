d41a8e6ace5ba91af9689f3c27cb504aa1fb36f5
max aguayo <maximiliano@dinametra.com>
2025-05-14 10:23:12 -0600

    fin de structs

--- structs/main.go
+++ structs/main.go
@@ -1,20 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
 
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
 	//alex := person{firstName: "alex", lastName: "anderson"}
 	//fmt.Println(alex)
 
-	var alex person
-	alex.firstName = "alex"
-	alex.lastName = "anderson"
-	fmt.Println(alex)
-	fmt.Printf("%+v", alex)
+	// var alex person
+	// alex.firstName = "alex"
+	// alex.lastName = "anderson"
+	// fmt.Println(alex)
+	// fmt.Printf("%+v", alex)
+
+	jim := person{
+		firstName: "jim",
+		lastName:  "party",
+		contact: contactInfo{
+			email:   "jim@gmail.com,",
+			zipCode: 94000,
+		},
+	}
+	//jimPointer := &jim
+	jim.updateName("jimmy")
+	jim.print()
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 
 }
